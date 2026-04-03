f6dc435974a434892d8ca80ec68bcaaa44559d2b
Pranav Raj Galipalli <galipallipranavraj@gmail.com>
2026-04-03 23:13:56 +0530

    fix: NCCL P2P/IB disable + NUMBA_THREADS=1 for stability

--- train_ddp.py
+++ train_ddp.py
@@ -30,6 +30,10 @@ def setup_ddp(rank, world_size):
     """Initializes the distributed process group."""
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12356'
+    # Stability Fixes for Jupyter/Shared environments
+    os.environ['NCCL_P2P_DISABLE'] = '1'
+    os.environ['NCCL_IB_DISABLE'] = '1'
+    os.environ['NUMBA_NUM_THREADS'] = '1'
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
